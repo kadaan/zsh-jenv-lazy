0dfd041209e8ad340976296eefe80a78d495cb48
Joel Baranick <joel.baranick@ensighten.com>
2018-06-12 14:35:10 -0700

    Fix the unset command

--- jenv-lazy.plugin.zsh
+++ jenv-lazy.plugin.zsh
@@ -4,7 +4,7 @@ if type jenv &> /dev/null; then
   export PATH="${JENV_SHIMS}:${PATH}"
   source $(which jenv)/../../libexec/completions/jenv.zsh
   function jenv() {
-    unset jenv
+    unset -f jenv > /dev/null 2>&1
     [[ -s "${HOME}/.jenv/bin/jenv-init.sh" ]] && source "${HOME}/.jenv/bin/jenv-init.sh" && source "${HOME}/.jenv/commands/completion.sh"
     eval "$(command jenv init -)"
     jenv "$@"
