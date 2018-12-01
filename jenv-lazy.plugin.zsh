d12ba4f34941ff8d79c26f59139ac2508bb4b0b7
Joel Baranick <joel.baranick@ensighten.com>
2018-12-01 01:42:46 -0800

    Fix lookup

--- jenv-lazy.plugin.zsh
+++ jenv-lazy.plugin.zsh
@@ -2,7 +2,7 @@
 if type jenv &> /dev/null; then
   local JENV_SHIMS="${JENV_ROOT:-${HOME}/.jenv}/shims"
   export PATH="${JENV_SHIMS}:${PATH}"
-  source $(which jenv)/../../libexec/completions/jenv.zsh
+  source $(whence -p jenv)/../../libexec/completions/jenv.zsh
   function jenv() {
     unset -f jenv > /dev/null 2>&1
     [[ -s "${HOME}/.jenv/bin/jenv-init.sh" ]] && source "${HOME}/.jenv/bin/jenv-init.sh" && source "${HOME}/.jenv/commands/completion.sh"
