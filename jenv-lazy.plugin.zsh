8e6643e4b7246a8510c1175266fe5fd2921d7fba
Joel Baranick <joel.baranick@ensighten.com>
2019-03-11 16:32:59 -0700

    Fix completions path

--- jenv-lazy.plugin.zsh
+++ jenv-lazy.plugin.zsh
@@ -2,7 +2,7 @@
 if type jenv &> /dev/null; then
   local JENV_SHIMS="${JENV_ROOT:-${HOME}/.jenv}/shims"
   export PATH="${JENV_SHIMS}:${PATH}"
-  source $(dirname $(greadlink -f `whence -p jenv`))/../completions/jenv.zsh
+  source $(dirname $(greadlink -f `whence -p jenv`))/../libexec/completions/jenv.zsh
   function jenv() {
     unset -f jenv > /dev/null 2>&1
     [[ -s "${HOME}/.jenv/bin/jenv-init.sh" ]] && source "${HOME}/.jenv/bin/jenv-init.sh" && source "${HOME}/.jenv/commands/completion.sh"
